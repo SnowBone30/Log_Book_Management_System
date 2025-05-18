00326628402a6d59f30b9e2e33dcf2edee82fbb0
SnowBone30 <d.jasten992@gmail.com>
2025-05-18 23:00:27 +0800

    Add files via upload

---
+++ LogiTrack/LogiTrack/MainMenu.Designer.vb
@@ -0,0 +1,105 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainMenu
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnLog = New System.Windows.Forms.Button()
+        Me.btnlogAdmin = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnLog
+        '
+        Me.btnLog.Location = New System.Drawing.Point(165, 206)
+        Me.btnLog.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.btnLog.Name = "btnLog"
+        Me.btnLog.Size = New System.Drawing.Size(266, 43)
+        Me.btnLog.TabIndex = 0
+        Me.btnLog.Text = "Staff/Visitors"
+        Me.btnLog.UseVisualStyleBackColor = True
+        '
+        'btnlogAdmin
+        '
+        Me.btnlogAdmin.Location = New System.Drawing.Point(165, 262)
+        Me.btnlogAdmin.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.btnlogAdmin.Name = "btnlogAdmin"
+        Me.btnlogAdmin.Size = New System.Drawing.Size(266, 43)
+        Me.btnlogAdmin.TabIndex = 1
+        Me.btnlogAdmin.Text = "Admin"
+        Me.btnlogAdmin.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(165, 314)
+        Me.btnExit.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(266, 43)
+        Me.btnExit.TabIndex = 2
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(266, 123)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(79, 20)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Welcome!"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(18, 14)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(78, 20)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "LogiTrack"
+        '
+        'MainMenu
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(650, 488)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnlogAdmin)
+        Me.Controls.Add(Me.btnLog)
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Name = "MainMenu"
+        Me.Text = "MainMenu"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnLog As Button
+    Friend WithEvents btnlogAdmin As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+End Class
---
+++ LogiTrack/LogiTrack/MainMenu.vb
@@ -0,0 +1,17 @@
+﻿Public Class MainMenu
+    Private Sub btnLog_Click(sender As Object, e As EventArgs) Handles btnLog.Click
+        Dim dashboard As New DeafaultDashboard
+        dashboard.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub btnlogAdmin_Click(sender As Object, e As EventArgs) Handles btnlogAdmin.Click
+        Dim defaultAdmin As New Form1
+        defaultAdmin.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
